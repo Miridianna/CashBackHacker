1fbe2e6db9f77e0fb5612edd95e9be3fa71f791f
Anna Yakovleva <akovlevaanna47@gmail.com>
2023-01-09 19:20:32 +0300

    Initial commit

--- src/main/java/ru/netology/unit/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.unit;
-
-public class Main {
-    public static void main(String[] args) {
-        BonusService servise = new BonusService();
-        int result = servise.calculateBonus(1_000);
-        System.out.println(result);
-    }
-}
